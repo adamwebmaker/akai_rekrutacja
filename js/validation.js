9ddc7dda78efd0665304c3bae4e895fe4661fd3e
adamwebmaker <47569331+adamwebmaker@users.noreply.github.com>
2021-10-22 17:22:13 +0200

    Add files via upload

--- js/validation.js
+++ js/validation.js
@@ -7,3 +7,19 @@
     Powodzenia!
     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 */
+function check(){
+  var imie = document.getElementById("first-name").value;
+  var nazwisko = document.getElementById("last-name").value;
+  var komunikat = document.getElementById("po");
+  if(imie.length==0 || nazwisko.length==0 || imie.search(["[0-9]"])!=-1 || nazwisko.search(["[0-9]"])!=-1){
+  komunikat.innerHTML="Wpisz poprawnie imie lub nazwisko.";
+  document.getElementById("sub").style.display = "none";
+  setTimeout(() => { komunikat.innerHTML=""; document.getElementById("sub").style.display = "block"; document.getElementById("sub").style.margin =" 20px auto" }, 5000);
+  }
+  else{
+    komunikat.innerHTML="Zgłoszenie zostało wysłane!";
+  }
+  if(!document.getElementById("frontend-checkbox").checked && !document.getElementById("frontend-checkbox").checked && !document.getElementById("frontend-checkbox").checked && !document.getElementById("frontend-checkbox").checked ){
+    komunikat.innerHTML+="</br>Wybierz chociaż jedną sekcje.";
+  }
+}
